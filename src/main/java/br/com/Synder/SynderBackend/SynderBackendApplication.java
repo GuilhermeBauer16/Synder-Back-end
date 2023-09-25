70f5ea8d55e4a31507e59ed9df82b080e3e8b0ad
Joao Pimentel <joao_pimentel98@hotmail.com>
2023-09-25 19:56:05 -0300

    feat: First Synder commit

---
+++ src/main/java/br/com/Synder/SynderBackend/SynderBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.Synder.SynderBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Synder/SynderBackend/SynderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Synder.SynderBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
