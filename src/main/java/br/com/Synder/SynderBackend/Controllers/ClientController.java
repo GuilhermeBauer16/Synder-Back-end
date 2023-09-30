c4658b78b6d9887194297ead8a8f743d3a232a51
Guilherme <guilhermebauer16@gmail.com>
2023-09-29 22:24:43 -0300

    feat: improved the Client controller

--- src/main/java/br/com/Synder/SynderBackend/Controllers/ClientController.java
+++ src/main/java/br/com/Synder/SynderBackend/Controllers/ClientController.java
@@ -1,40 +1,36 @@
 package br.com.Synder.SynderBackend.Controllers;
 
 
-
-import br.com.Synder.SynderBackend.domain.client.Client;
-import br.com.Synder.SynderBackend.domain.client.ClientRepository;
-import br.com.Synder.SynderBackend.domain.client.RequestClientDTO;
-import br.com.Synder.SynderBackend.domain.client.UpdateClientDTO;
+import br.com.Synder.SynderBackend.domain.client.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
-
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/client")
 public class ClientController {
     @Autowired
     private ClientRepository repository;
-    @Autowired
-   // private ClientService clientService;
-   @GetMapping
-    public ResponseEntity getAllClients(){
-       var allClients = repository.findAll();
-       return ResponseEntity.ok(allClients);
-   }
 
-   @PostMapping
-   @Transactional
-    public ResponseEntity registerClient(@RequestBody @Valid RequestClientDTO data){
+    @Autowired
+    // private ClientService clientService;
+    @GetMapping
+    public ResponseEntity getAllClients() {
+        var allClients = repository.findAll();
+        return ResponseEntity.ok(allClients);
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity registerClient(@RequestBody @Valid RequestClientDTO data, UriComponentsBuilder uriBuilder) {
         Client newClient = new Client(data);
         repository.save(newClient);
-        return ResponseEntity.ok().build();
-   }
+        var uri = uriBuilder.path("/client/{id}").buildAndExpand(newClient.getId()).toUri();
+        return ResponseEntity.created(uri).body(new ClientDetail(newClient));
+    }
 
     @PutMapping
     @Transactional
@@ -42,12 +38,12 @@ public class ClientController {
 
         var client = repository.getReferenceById(updateClientDTO.id());
         client.clientUpdate(updateClientDTO);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new ClientDetail(client));
     }
 
-   @DeleteMapping("/{id}")
-    public ResponseEntity deleteClient(@PathVariable Long id){
-       repository.deleteById(id);
-       return ResponseEntity.noContent().build();
-   }
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteClient(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
---
+++ src/main/java/br/com/Synder/SynderBackend/infra/exception/ErrorHandling.java
@@ -0,0 +1,31 @@
+package br.com.Synder.SynderBackend.infra.exception;
+
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandling {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleError400(MethodArgumentNotValidException exception){
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map( ValidationErrorData:: new ).toList());
+    }
+
+    public  record  ValidationErrorData(String field , String message){
+
+        public  ValidationErrorData(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
